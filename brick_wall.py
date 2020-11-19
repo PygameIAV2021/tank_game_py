f5bd23c4cd0549078759516057dd4a3b7ec467de
wit-a <62643563+wit-a@users.noreply.github.com>
2020-11-19 12:01:47 +0100

    wird erst mal zurückgestelt

---
+++ brick_wall.py
@@ -0,0 +1,24 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                               #
+#                                     Rudolf-Diesel-Fachschule                                  #
+#                                        script programming                                     #
+#                                               wit-a                                           #
+#                                             Tank Game                                         #
+#                                                                                               #                              
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #                                                                      
+#                                                                                               #
+#                                       File: Brick_Wall.py                                     #
+#                                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class Brick_Wall:
+    """ 
+        Brick Wall with health 
+    """
+    health = 1
+
+    def hit(self, damage):
+        
+        self.health -= damage
+
+        return self.health
